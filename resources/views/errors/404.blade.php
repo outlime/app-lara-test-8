e748ea4e8cc697a3d0e2166c1bb4e7d4ebcb6ac2
I'm a Czarina <salvadorjoyczarina@gmail.com>
2015-09-27 06:46:22 +0800

    Error fronts

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 @extends('errors.error')
 
 @section('content')
-	
+	<i class="fa fa-frown-o fa-5x"></i>
 	<h1>You're trying to visit a lost kingdom.</h1>
 	<a href="/" class="btn btn-lg btn-primary">Take me back home!</a>
 
--- resources/views/errors/error.blade.php
+++ resources/views/errors/error.blade.php
@@ -7,15 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title>Pastiche</title>
-
+	<!--DOWNLOAD THIS-->
+	<link href='https://fonts.googleapis.com/css?family=Raleway:100' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	
 	<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
-	<style type="text/css">
-
+	<!--Palipat sa iba-->
+	<style>
 		body {
 			padding-top: 150px;
 			min-height: 500px;
+			font-family: "Raleway", sans-serif;
+			text-align: center;
 		}
-
 	</style>
 </head>
 <body>
