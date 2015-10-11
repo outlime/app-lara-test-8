e961a3bcbac095e7680b0bbe67ff5be5733adadb
Emil Matubis <outlime@users.noreply.github.com>
2015-10-12 00:14:13 +0800

    Pastiche

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,16 +4,7 @@
 <div class="login-register">
 	<h1>Register</h1>
 
-	@if (count($errors) > 0)
-		<div class="alert alert-danger">
-			<strong>Whoops!</strong> There were some problems with your input.<br><br>
-			<ul>
-				@foreach ($errors->all() as $error)
-					<li>{{ $error }}</li>
-				@endforeach
-			</ul>
-		</div>
-	@endif
+	@include('partials.messages')
 
     <form method="POST" action="/auth/register">
     	<input type="hidden" name="_token" value="{{ csrf_token() }}">
