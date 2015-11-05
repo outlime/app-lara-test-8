6c80f689658ba202abbf0d1137518af0219006c6
Emil Matubis <outlime@users.noreply.github.com>
2015-11-06 04:47:14 +0800

    Pastiche

--- resources/views/user/search.blade.php
+++ resources/views/user/search.blade.php
@@ -14,31 +14,30 @@
 					@foreach ($results as $user)
 						@unless (Auth::user()->id == $user->id)
 							<div class="list-group-item">
-								<div class="row">
-									<div class="col-md-9">
-										<h4><a href="{{ $user->username }}">{{ $user->name }}</a></h4>
-									</div>	
-
-									<div class="col-md-3">
-										<p>
-											@unless (Auth::user()->isFollowing($user))
-												<a class="btn btn-pastiche" href="{{ $user->username }}/follow">
-													<i class="fa fa-plus"></i> Follow
-												</a>
+		                        <div class="media">
+		                            <div class="media-left">
+		                                <img class="profile-pic-sm media-object" src="{{ URL::asset('images/usm/' . $user->profile_pic) }}" alt="">
+		                            </div>
+		                            <div class="media-body">
+		                                @unless (Auth::user()->isFollowing($user))
+						                   <a type="button" class="close btn btn-pastiche" data-toggle="tooltip" data-placement="left" title="Follow {{ $user->name}}" href="{{ $user->username }}/follow"><i class="fa fa-plus"></i></a>
+										@endif
+		                                <h4 class="media-heading">
+		                                    <a href="/{{ $user->username }}">
+		                                        {{ $user->name }}
+		                                    </a>
+		                                </h4>
+		                                <h5 class="media-body">
+        									@if (count($user->followers) == 0)
+												No Followers
+											@elseif (count($user->followers) == 1)
+												{{ count($user->followers) }} Follower
+											@else
+												{{ count($user->followers) }} Followers
 											@endif
-										</p>
-									</div>
-								</div>
-
-								<p class="list-group-item-text">
-									@if (count($user->followers) == 0)
-										No Followers
-									@elseif (count($user->followers) == 1)
-										{{ count($user->followers) }} Follower
-									@else
-										{{ count($user->followers) }} Followers
-									@endif
-								</p>
+		                                </h5>
+		                            </div>
+		                        </div>
 							</div>
 						@endunless
 					@endforeach
